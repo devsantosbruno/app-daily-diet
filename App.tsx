84d4c96aa32384f2672e28b3c7923d9b4219d95b
bruno santos <devbrunosantos@gmail.com>
2023-09-21 00:08:24 -0300

    feat: roboto font and Loading component

--- App.tsx
+++ App.tsx
@@ -1,5 +1,29 @@
+import { StatusBar } from "react-native";
 import { Routes } from "./src/routes";
+import { ThemeProvider } from "styled-components/native";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+
+import theme from "./src/theme";
+
+import { Loading } from "@components/Loading";
 
 export default function App() {
-  return <Routes />;
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        {fontsLoaded ? <Routes /> : <Loading />}
+      </>
+    </ThemeProvider>
+  );
 }
