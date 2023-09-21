0b9cc54ae1048285ae4368a455113cd7f563918a
bruno santos <devbrunosantos@gmail.com>
2023-09-21 00:25:47 -0300

    feat: navigation type

---
+++ src/@types/navigation.d.ts
@@ -0,0 +1,12 @@
+export declare global {
+  namespace ReactNavigation {
+    interface RootParamList {
+      Home: undefined;
+      PersonalPerformance: undefined;
+      RegisterFood: undefined;
+      GoodFoodRegistered: undefined;
+      BadFoodRegistered: undefined;
+      EditOrDeleteFoodRegistered: undefined;
+    }
+  }
+}
